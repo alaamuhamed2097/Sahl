259b671c169f3d7986a8a47e4b5b8e75ef435ee2
ITLEGEND <ITLEGEND@DESKTOP-IL4NJLO>
2025-11-04 11:38:13 +0200

    Feature: Complete Location cycle

--- src/Presentation/Dashboard/Pages/Location/City/Index.razor.cs
+++ src/Presentation/Dashboard/Pages/Location/City/Index.razor.cs
@@ -1,47 +1,54 @@
-namespace UI.Pages.Location.City
+using Dashboard.Constants;
+using Dashboard.Contracts.Location;
+using Microsoft.AspNetCore.Components;
+using Resources;
+using Shared.DTOs.Location;
+using Shared.GeneralModels;
+
+namespace Dashboard.Pages.Location.City
 {
-    //public partial class City : BaseListPage<CityDto>
-    //{
-    //    protected override string EntityName { get; } = "City";
-    //    protected override string AddRoute { get; } = "/city";
-    //    protected override string EditRouteTemplate { get; } = $"/city/{{id}}";
-    //    protected override string SearchEndpoint { get; } = ApiEndpoints.City.Search;
-    //    protected override Dictionary<string, Func<CityDto, object>> ExportColumns { get; }
-    //    = new Dictionary<string, Func<CityDto, object>>
-    //    {
-    //        [ECommerceResources.Title] = x => x.Title,
-    //    };
+    public partial class Index : BaseListPage<CityDto>
+    {
+        protected override string EntityName { get; } = "City";
+        protected override string AddRoute { get; } = "/city";
+        protected override string EditRouteTemplate { get; } = $"/city/{{id}}";
+        protected override string SearchEndpoint { get; } = ApiEndpoints.City.Search;
+        protected override Dictionary<string, Func<CityDto, object>> ExportColumns { get; }
+        = new Dictionary<string, Func<CityDto, object>>
+        {
+            [ECommerceResources.Title] = x => x.Title,
+        };
 
-    //    [Inject] protected ICityService CityService { get; set; } = null!;
+        [Inject] protected ICityService CityService { get; set; } = null!;
 
-    //    protected override async Task<ResponseModel<IEnumerable<CityDto>>> GetAllItemsAsync()
-    //    {
-    //        var result = await CityService.GetAllAsync();
-    //        if (result.Success)
-    //        {
-    //            return result;
-    //        }
-    //        else
-    //        {
-    //            return result;
-    //        }
-    //    }
-    //    protected override async Task<ResponseModel<bool>> DeleteItemAsync(Guid id)
-    //    {
-    //        return await CityService.DeleteAsync(id);
-    //    }
-    //    protected override async Task<string> GetItemId(CityDto item)
-    //    {
-    //        var result = await CityService.GetByIdAsync(item.Id);
-    //        if (result.Success)
-    //        {
-    //            return result.Data?.Id.ToString() ?? string.Empty;
-    //        }
-    //        else
-    //        {
-    //            await ShowErrorNotification(NotifiAndAlertsResources.Error, NotifiAndAlertsResources.NoDataFound);
-    //            return string.Empty;
-    //        }
-    //    }
-    //}
+        protected override async Task<ResponseModel<IEnumerable<CityDto>>> GetAllItemsAsync()
+        {
+            var result = await CityService.GetAllAsync();
+            if (result.Success)
+            {
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+        protected override async Task<ResponseModel<bool>> DeleteItemAsync(Guid id)
+        {
+            return await CityService.DeleteAsync(id);
+        }
+        protected override async Task<string> GetItemId(CityDto item)
+        {
+            var result = await CityService.GetByIdAsync(item.Id);
+            if (result.Success)
+            {
+                return result.Data?.Id.ToString() ?? string.Empty;
+            }
+            else
+            {
+                await ShowErrorNotification(NotifiAndAlertsResources.Error, NotifiAndAlertsResources.NoDataFound);
+                return string.Empty;
+            }
+        }
+    }
 }
--- src/Presentation/Dashboard/Pages/Location/Country/Index.razor.cs
+++ src/Presentation/Dashboard/Pages/Location/Country/Index.razor.cs
@@ -1,56 +1,54 @@
-//using Microsoft.AspNetCore.Components;
-//using Microsoft.JSInterop;
-//using Resources;
-//using Shared.DTOs.User;
-//using Shared.GeneralModels;
-//using Shared.GeneralModels.SearchCriteriaModels;
-//using UI.Constants;
-//using UI.Contracts;
+using Dashboard.Constants;
+using Dashboard.Contracts;
+using Microsoft.AspNetCore.Components;
+using Resources;
+using Shared.DTOs.Location;
+using Shared.GeneralModels;
 
-//namespace UI.Pages.Location.Country
-//{
-//    public partial class Countries : BaseListPage<CountryDto>
-//    {
-//        protected override string EntityName { get; } = "Country";
-//        protected override string AddRoute { get; } = "/country";
-//        protected override string EditRouteTemplate { get; } = $"/country/{{id}}";
-//        protected override string SearchEndpoint { get; } = ApiEndpoints.Country.Search;
-//        protected override Dictionary<string, Func<CountryDto, object>> ExportColumns { get; }
-//        = new Dictionary<string, Func<CountryDto, object>>
-//        {
-//            [ECommerceResources.Title] = x => x.Title,
-//        };
+namespace Dashboard.Pages.Location.Country
+{
+    public partial class Index : BaseListPage<CountryDto>
+    {
+        protected override string EntityName { get; } = "Country";
+        protected override string AddRoute { get; } = "/country";
+        protected override string EditRouteTemplate { get; } = $"/country/{{id}}";
+        protected override string SearchEndpoint { get; } = ApiEndpoints.Country.Search;
+        protected override Dictionary<string, Func<CountryDto, object>> ExportColumns { get; }
+        = new Dictionary<string, Func<CountryDto, object>>
+        {
+            [ECommerceResources.Title] = x => x.Title,
+        };
 
-//        [Inject] protected ICountryService CountryService { get; set; } = null!;
+        [Inject] protected ICountryService CountryService { get; set; } = null!;
 
-//        protected override async Task<ResponseModel<IEnumerable<CountryDto>>> GetAllItemsAsync()
-//        {
-//            var result = await CountryService.GetAllAsync();
-//            if (result.Success)
-//            {
-//                return result;
-//            }
-//            else
-//            {
-//                return result;
-//            }
-//        }
-//        protected override async Task<ResponseModel<bool>> DeleteItemAsync(Guid id)
-//        {
-//            return await CountryService.DeleteAsync(id);
-//        }
-//        protected override async Task<string> GetItemId(CountryDto item)
-//        {
-//            var result = await CountryService.GetByIdAsync(item.Id);
-//            if (result.Success)
-//            {
-//                return result.Data?.Id.ToString() ?? string.Empty;
-//            }
-//            else
-//            {
-//                await ShowErrorNotification(NotifiAndAlertsResources.Error, NotifiAndAlertsResources.NoDataFound);
-//                return string.Empty;
-//            }
-//        }
-//    }
-//}
+        protected override async Task<ResponseModel<IEnumerable<CountryDto>>> GetAllItemsAsync()
+        {
+            var result = await CountryService.GetAllAsync();
+            if (result.Success)
+            {
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+        protected override async Task<ResponseModel<bool>> DeleteItemAsync(Guid id)
+        {
+            return await CountryService.DeleteAsync(id);
+        }
+        protected override async Task<string> GetItemId(CountryDto item)
+        {
+            var result = await CountryService.GetByIdAsync(item.Id);
+            if (result.Success)
+            {
+                return result.Data?.Id.ToString() ?? string.Empty;
+            }
+            else
+            {
+                await ShowErrorNotification(NotifiAndAlertsResources.Error, NotifiAndAlertsResources.NoDataFound);
+                return string.Empty;
+            }
+        }
+    }
+}
