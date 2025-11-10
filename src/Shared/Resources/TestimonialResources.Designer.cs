788dbf92d7fee6119a38359695e63cec147ad555
mohammed <mohasobhy6@gmail.com>
2025-11-10 18:08:51 +0200

    hotfix / update for shippingCompanies

--- src/Shared/Resources/TestimonialResources.Designer.cs
+++ src/Shared/Resources/TestimonialResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class TestimonialResources {
@@ -196,7 +196,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to إدارة شهادات العملاء.
+        ///   Looks up a localized string similar to شهادات العملاء.
         /// </summary>
         public static string TestimonialManagement {
             get {
