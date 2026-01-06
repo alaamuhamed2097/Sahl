9eba868d2b92f16160b26e45a510d533b348ad31
Alaa Mohamed <alaamuhamed2097@gmail.com>
2026-01-06 12:50:11 +0200

    bug fix: fix home page - search - item details

--- src/Infrastructure/DAL/Migrations/20260106092301_addSpSearchItemsMultiVendorEstimatedDeliveryDays.Designer.cs
+++ src/Infrastructure/DAL/Migrations/20260106092301_addSpSearchItemsMultiVendorEstimatedDeliveryDays.Designer.cs
@@ -4,6 +4,7 @@ using DAL.ApplicationContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260106092301_addSpSearchItemsMultiVendorEstimatedDeliveryDays")]
+    partial class addSpSearchItemsMultiVendorEstimatedDeliveryDays
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -7307,9 +7310,6 @@ namespace DAL.Migrations
                     b.Property<int>("AvailableQuantity")
                         .HasColumnType("int");
 
-                    b.Property<decimal?>("AverageRating")
-                        .HasColumnType("decimal(18,2)");
-
                     b.Property<Guid?>("BrandId")
                         .HasColumnType("uniqueidentifier");
 
@@ -7336,8 +7336,8 @@ namespace DAL.Migrations
                     b.Property<Guid>("ItemId")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<Guid>("OfferCombinationPricingId")
-                        .HasColumnType("uniqueidentifier");
+                    b.Property<decimal?>("ItemRating")
+                        .HasColumnType("decimal(18,2)");
 
                     b.Property<decimal>("Price")
                         .HasColumnType("decimal(18,2)");
@@ -7368,9 +7368,6 @@ namespace DAL.Migrations
                     b.Property<string>("TitleEn")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("TotalRecords")
-                        .HasColumnType("int");
 
                     b.ToTable((string)null);
 
